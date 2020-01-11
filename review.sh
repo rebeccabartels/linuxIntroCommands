b5d89d5e5dae9beac2b051bfcd50d78b4aff1db4
riley <rebeccakbartels@gmail.com>
2020-01-11 10:56:30 -0500

    review

---
+++ review.sh
@@ -0,0 +1,67 @@
+## General Linux Review
+### Instructions
+In this activity, you will complete a review of core Linux concepts.
+- You can work on this activity with a partner. However, feel free to work by yourself if you wish to gauge your Linux knowledge.
+- Take your time and be thorough when answering each question. Once you have finished, be prepared to be called on to answer questions during our review.
+### Questions
+- What are some Text Editors?
+nano, vim
+- What is the Top of the Linux file structure?
+the root directory 
+- Name the directory that contains:
+  - Binaries
+bin
+  - Files associated to the Kernel
+/boot
+  - Log Files
+/var
+  - Temporary Files
+/cdrom
+  - Configuration Files
+/etc 
+  - Process Files
+/proc 
+  - Files the user wants to save
+/documents 
+- Name the command you use to:
+  - Install a package
+apt-get install 
+  - Add a user (*Note*: Name two commands.)
+add user
+user add 
+  - Change a password
+su passwd
+  - Create a neww group
+groupadd 
+  - Add a user to a group
+usermod -a -G examplegroup exampleusername
+  - Check which groups you’re in
+groups 
+  - Find your user ID
+usermod -u 1004 user_2 
+- What are the 3 types of file permissions?
+r, w, ex
+- Which command displays a file’s permissions?
+chmod 
+- Which file gets modified when a user is added to the system?
+
+- Which file contains hashed passwords?
+
+- How can you tell which algorithm was used to hash a password in the file above?
+
+- Which command changes file permissions?
+
+- What are the 2 types of ways to change file permissions?
+
+- What is the command to change ownership permissions?
+
+- How do you edit the sudoers file?
+
+- How do you use sudo?
+sudo
+- What is the command to switch to another user?
+su
+- What is the command to archive files?
+tar 
+- What is the command to view running processes?
+netstat
